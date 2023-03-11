3a778540b1c6b4490eddb1a53bd9796f747df520
yingxu.fan@cloudpense.com <baiyangmu333@163.com>
2023-03-11 14:02:13 +0800

    title:建立工程

---
+++ src/main/java/com/example/memotask/MemoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.memotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/memotask/MemoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
