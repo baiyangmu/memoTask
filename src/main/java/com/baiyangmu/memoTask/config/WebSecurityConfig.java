b697c2c53878c2645ae07e534d59fc022a162255
yingxu.fan@cloudpense.com <baiyangmu333@163.com>
2023-03-12 22:31:53 +0800

    title:搭建主数据框架 description: 完成的功能： 1、除了登录、注册和首页意外接口的解析token的功能 2、登出功能

--- src/main/java/com/baiyangmu/memoTask/config/WebSecurityConfig.java
+++ src/main/java/com/baiyangmu/memoTask/config/WebSecurityConfig.java
@@ -1,31 +1,30 @@
-//package com.baiyangmu.memotask.config;
-//
-////import com.baiyangmu.memotask.filter.JwtTokenFilter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////    @Autowired
-////    private JwtTokenFilter jwtTokenFilter;
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/user/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/user/login").permitAll()
-//                .and()
-//                .logout().permitAll();
-//
-//        //http.addFilterBefore(jwtTokenFilter, AbstractPreAuthenticatedProcessingFilter.class);
-//    }
-//
-//}
-//
+package com.baiyangmu.memotask.config;
+
+import com.baiyangmu.memotask.filter.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenFilter filter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/user/login" , "/user/registeredUser").permitAll()
+                .anyRequest().permitAll();
+
+
+        http.addFilterBefore(filter, BasicAuthenticationFilter.class);
+    }
+
+}
+
